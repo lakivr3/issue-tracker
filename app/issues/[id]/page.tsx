041f429a596332d4280bde39febf846cb56d0058
lakivr3 <127746116+lakivr3@users.noreply.github.com>
2024-01-12 21:07:34 +0100

    Details

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,29 @@
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import ReactMarkdown from "react-markdown";
+
+interface Props {
+  params: { id: string };
+}
+
+export default async function IssueDetailPage({ params }: Props) {
+  //   if (typeof parseInt(params.id) !== "number") notFound();
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue) notFound();
+  return (
+    <div>
+      <Heading>{issue.title}</Heading>
+      <Flex gap={"3"} my={"2"}>
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className="pros " mt={"4"}>
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
+    </div>
+  );
+}
--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,19 +1,15 @@
+import { Table } from "@radix-ui/themes";
 import React from "react";
-import prisma from "@/prisma/client";
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
 import IssueActions from "./IssueActions";
 
-export default async function IssuesPage() {
-  const issues = await prisma.issue.findMany();
-  await delay(2000);
-
+export default function LoadingIssuesPage() {
+  const issues = [1, 2, 3, 4, 5];
   return (
     <div>
       <IssueActions />
-
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
@@ -28,18 +24,18 @@ export default async function IssuesPage() {
         </Table.Header>
         <Table.Body>
           {issues.map((issue) => (
-            <Table.Row key={issue.id}>
+            <Table.Row key={issue}>
               <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                <Skeleton />
                 <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
+                  <Skeleton />
                 </div>
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
+                <Skeleton />
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                {issue.updatedAt.toDateString()}
+                <Skeleton />
               </Table.Cell>
             </Table.Row>
           ))}
