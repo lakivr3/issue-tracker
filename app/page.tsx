bd51342543f74379c8a5fadb90deff800257278f
lakivr3 <127746116+lakivr3@users.noreply.github.com>
2024-03-02 20:44:13 +0100

    xd

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
-import { Button } from "@radix-ui/themes";
-import Link from "next/link";
+import { Flex, Grid } from "@radix-ui/themes";
+import IssueChart from "./IssueChart";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
+import LatestIssues from "./LatestIssues";
 
-export default function Home() {
+interface Props {
+  searchParams: { page: string };
+}
+
+export default async function Home({ searchParams }: Props) {
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+
   return (
-    <div>
-      <Button>
-        <Link href="/issues/new">New Issue</Link>
-      </Button>
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary closed={closed} inProgress={inProgress} open={open} />
+        <IssueChart closed={closed} inProgress={inProgress} open={open} />
+      </Flex>
+      <LatestIssues />
+    </Grid>
   );
 }
